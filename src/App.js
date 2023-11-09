1aabe3573f7916f6a75a798865689600abc4a7a1
Bharat Sharma <bharatsharma98971@gmail.com>
2023-11-09 14:54:33 +0530

    Commit 2.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
+import {  BrowserRouter as Router,Routes,Route } from "react-router-dom"; 
 import './App.css';
+import Home from './pages/Student/Home';
+import AdminLogin from "./pages/Admin/Login";
+import StudentLogin from "./pages/Student/Login";
+import PostMember from "./pages/Admin/PostMember";
+import Members from "./pages/Student/Members";
+import PostEvents from "./pages/Admin/PostEvents";
+import Register from "./pages/Student/Register";
+import Event from "./pages/Student/Event";
+import EventDetailed from "./pages/Student/EventDetailed";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/members" element={<Members/>} />
+        <Route path="/student/register" element={<Register/>}/>
+        <Route path="/student/login" element={<StudentLogin/>}/>
+        <Route path="/events" element={<Event/>}/>
+        <Route path="/events/:id" element={<EventDetailed/>}/>
+        {/* Admin Routes */}
+        <Route path="/admin/login" element={<AdminLogin/>} />
+        <Route path="/admin/postmember" element={<PostMember/>}/>
+        <Route path="/admin/postevents" element={<PostEvents/>}/>
+        {/* Universal Route */}
+        <Route path="*" element={<Home/>} />
+      </Routes>
+    </Router>
   );
 }
 
